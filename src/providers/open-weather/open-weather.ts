032b52e3cce2d30575a96074d92719456b7e7815
nkosazane <nkosazanembokazi@gmail.com>
2019-08-19 10:36:32 +0200

    weather app

--- src/providers/open-weather/open-weather.ts
+++ src/providers/open-weather/open-weather.ts
@@ -15,7 +15,7 @@ export class OpenWeatherProvider {
   }
 
   updateWeather(city){
-    return this.http.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=ec0da5acb58d4cf1690ff8735cdb73e7');
+    return this.http.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=');
   }
 
 }
